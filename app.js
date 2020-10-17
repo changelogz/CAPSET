0bf2cb959d79d0f98b1748b1297d0553fa628b12
Yutolab1120 <65591782+Yuto210-EN@users.noreply.github.com>
2020-10-18 04:48:23 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -5,54 +5,26 @@ const io = require('socket.io')(http);
 const PORT = process.env.PORT || 7000;
 
 
-
-// express "/" setting
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/home/index.html');
-});
-
-app.get('/chat', function (req, res) {
-    res.sendFile(__dirname + '/chat.html');
-});
-
+// express()
 app.get('/quest', function (req, res) {
     res.sendFile(__dirname + '/quest.html');
 });
 
-app.get('/front', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
-});
-
-app.get('/front/lives', function (req, res) {
-    res.sendFile(__dirname + '/live/index.html');
-});
-
-
-
-// socket.io RealtimeChat
-io.on('connection', function (socket) {
-    socket.on('message', function (msg) {
-        console.log('message: ' + msg);
-        io.emit('message', msg);
-    });
-});
-
-// socket.io QuestTitle(1)
+// socket.io (1) QuestTitle
 io.on('connection', function (socket) {
     socket.on('message_1', function (msg_1) {
         console.log('message_1: ' + msg_1);
         io.emit('message_1', msg_1);
     });
 });
-//socket.io Quest-Sub(2)
+
+//socket.io (2) SubQuest
 io.on('connection', function (socket) {
     socket.on('message_2', function (msg_2) {
         console.log('message_2: ' + msg_2);
         io.emit('message_2', msg_2);
     });
 });
-
-
 
 // http_service
 http.listen(PORT, function () {
